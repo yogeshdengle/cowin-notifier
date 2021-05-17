baf220939ef0ea8cbf62d2e15657023ca2cad894
Yogesh Dengle <yogeshdengle@gmail.com>
2021-05-18 00:17:08 +0530

    First cut for handling lambda based notifiers

--- src/index.js
+++ src/index.js
@@ -1,220 +1,191 @@
-const https = require('https');
-const config = require('../resources/config.json');
 const got = require('got');
+const mysql = require('mysql2/promise');
 
-const fs = require('fs');
-const { TelegramClient } = require('telegram');
-const { StringSession } = require('telegram/sessions');
-const { Logger } = require("telegram/extensions");
-Logger.setLevel("error");
+const apiURL = process.env.URL;
+const A45BotId = process.env.A45_BOT_ID;
+const U45BotId = process.env.U45_BOT_ID;
+const A45GroupId = process.env.A45_GROUP_ID;
+const U45GroupId = process.env.U45_GROUP_ID;
+const districtId = process.env.DISTRICT_ID;
 
-const apiURL = config.url;
-const apiId = config.apiId;
-const apiHash = config.apiHash;
-const BotToken = config.botToken;
-var client;
-var notificationSentCenters = new Set();
-var notificationsSentPerSessionPer24Hrs = new Map();
-var log = console.log;
-var responseCache = new Map();
+let client;
+let connection;
+let isInited;
 
-console.log = function () {
-  var first_parameter = arguments[0];
-  var other_parameters = Array.prototype.slice.call(arguments, 1);
 
-  function formatConsoleDate(date) {
-    var hour = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var milliseconds = date.getMilliseconds();
 
-    return '[' +
-      ((hour < 10) ? '0' + hour : hour) +
-      ':' +
-      ((minutes < 10) ? '0' + minutes : minutes) +
-      ':' +
-      ((seconds < 10) ? '0' + seconds : seconds) +
-      '.' +
-      ('00' + milliseconds).slice(-3) +
-      '] ';
+
+async function init() {
+  try {
+    console.log("Initing Lambda function");
+    var initMySQL = process.env.MYSQL_INIT || 'true';
+    initMySQL = (initMySQL == 'true');
+    //Update connection parameters based on your connection string.
+    if (initMySQL) {
+      connection = await mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PSWD,
+        database: process.env.MYSQL_DB
+      });
+
+      //you can use either prepared statements or direct queries, if multiple queries are reused all the time prefer prepared statements.
+      //preparedEmpStatement = await connection.prepare("select * from employees where emp_no = ?");
+      //preparedEmpInsertStatement = await connection.prepare("insert into employees values (?,?,?,?,?,?)");
+      //preparedEmpUpdateStatement = await connection.prepare("update employees set gender=? where emp_no =?");
+    }
+    isInited = true;
+  } catch (e) {
+    console.log("Error while initing the lambda", e);
   }
+}
 
-  log.apply(console, [formatConsoleDate(new Date()) + first_parameter].concat(other_parameters));
-};
 
 
 function getTodaysDate() {
   const now = new Date();
   let month = now.getMonth() + 1;
-  let date = now.getDay();
+  let date = now.getDate();
   let year = now.getFullYear();
-  return date + "-" + month + "-" + year;
+  return ('0' + now.getDate()).slice(-2)+'-'+('0' + (now.getMonth()+1)).slice(-2)+'-'+now.getFullYear();
+  //return date + "-" + month + "-" + year;
 }
 
-async function getDistrictData(district_id) {
-  console.log("Getting data for District " + district_id);
-  //let url = new URL(apiURL);
+async function getDistrictData() {
+  console.log("Getting data for District " + districtId);
   let searchParams = {
-    "district_id":  district_id,
+    "district_id":  districtId,
     "date" : getTodaysDate()
   }
 
+  var hook =  {
+		beforeRequest: [
+			req => {
+                //console.log(JSON.stringify(req));
+			}
+		]
+	};
   let options = {
     method: "GET",
     headers: {
-      "Accept-Language": "hl_IN",
       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0",
-      "DNT": "1",
-      "Referer": "https://www.cowin.gov.in/",
-      "Origin" : "https://www.cowin.gov.in/",
-      "Accept-Language": "en-US,en;q=0.5"
+      //"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15",
+      "DNT": "1"
     },
     searchParams,
     http2 :true,
-    //cache : responseCache,
-    responseType: 'json'
-    
-  }
+    responseType: 'json',
+    hooks: hook
+  };
+  
   return  await got(apiURL, options);
 }
-
-
-
 
 function isDateGreaterThanEqualToToday(date) {
   const now = new Date();
   now.setHours(0, 0, 0, 0)
-  const passedDate = new Date(date);
+  const passedDate = new Date();
+  passedDate.setHours(0,0,0,0);
+  var fields = date.split('-');
+  passedDate.setDate(Number.parseInt(fields[0]));
+  passedDate.setFullYear(Number.parseInt(fields[2]));
+  passedDate.setMonth(Number.parseInt(fields[1])-1);
   return passedDate >= now;
 }
 
-async function filterAvailable(data, groupName, age, filterFunction) {
-  if (typeof filterFunction !== 'function'){
-      filterFunction = (center, session) => {
-        return (session.min_age_limit == age && session.available_capacity > 0 );
-      }
-  }
+async function filterAvailableAndSendNotifications(data) {
   for (let center of data.centers) {
     for (let session of center.sessions) {
-      if (filterFunction(center, session) && isDateGreaterThanEqualToToday(session.date) &&
-      !notificationSentCenters.has(session.session_id) 
-      && (!notificationsSentPerSessionPer24Hrs.get(session.session_id) 
-          || notificationsSentPerSessionPer24Hrs.get(session.session_id) < 3)) {
-        let notificationData = {
-          sessionId: session.session_id,
-          state: center.state_name,
-          dist: center.district_name,
-          pin: center.pincode,
-          name: center.name,
-          blockName: center.block_name,
-          date: session.date,
-          slots: session.available_capacity,
-          freePaid: center.fee_type,
-          foundAt: new Date().toLocaleString(),
-          vaccine: session.vaccine,
-          address: center.address
-        }
-        await sendNotification(notificationData, groupName, age);
-        notificationSentCenters.add(notificationData.sessionId);
+      if ((session.available_capacity_dose1 > 0 || session.available_capacity_dose2 > 0) 
+          && isDateGreaterThanEqualToToday(session.date)){
+        let slotData = createSlotData(center, session);
+        await sendNotification(slotData);
+        await postNotificationHandle(slotData);
       }
     }
   }
 }
 
-async function sendNotification(data, groupName, age) {
+async function postNotificationHandle(slotData){
+  //do your SQL magic here.
+  return;
+}
+
+function createSlotData(center, session){
+  let notificationData = {
+    sessionId: session.session_id,
+    centerId: center.center_id,
+    state: center.state_name,
+    dist: center.district_name,
+    pin: center.pincode,
+    name: center.name,
+    blockName: center.block_name,
+    date: session.date,
+    freePaid: center.fee_type,
+    foundAt: new Date().toLocaleString(),
+    vaccine: session.vaccine,
+    address: center.address,
+    age : session.min_age_limit,
+    totalCapacity : session.available_capacity,
+    dose1Capacity : session.available_capacity_dose1,
+    dose2Capacity : session.available_capacity_dose2
+  }
+  return notificationData;
+}
+
+async function sendNotification(slotData) {
   try {
-    let notificationString = `Found ${data.slots} open ${age}+ slots at center "${data.name}" Address - "${data.address}" Block/Tal - "${data.blockName}" pin code is ${data.pin} for date ${data.date}. \nAvailable Vaccine is ${data.vaccine}. This is a ${data.freePaid} site.\nThis data is as of ${data.foundAt}. \n\nRegistration Link: https://selfregistration.cowin.gov.in/ \n\nThe API used to fetch data is sometimes inconsistent, please reverify this against the Cowin website.`;
-    console.log("SessionId-%s is in notifications %s ; 24 hrs count %s and notification is \n %s", data.sessionId, notificationSentCenters.has(data.sessionId), notificationsSentPerSessionPer24Hrs.get(data.sessionId), notificationString);
-    await client.sendMessage(groupName, { message: notificationString });
+    let message = `Age: ${slotData.age}+\nName: ${slotData.name} \n 
+Address: ${slotData.address}
+Block/Tal/Area: ${slotData.blockName}
+PinCode: ${slotData.pin}
+Date: ${slotData.date} 
+Vaccine: ${slotData.vaccine}
+Free/Paid: ${slotData.freePaid}
+
+Total Capacity:  ${slotData.totalCapacity} 
+Dose1 Capacity: ${slotData.dose1Capacity}
+Dose2 Capacity: ${slotData.dose2Capacity}
+\nRegistration Link: https://selfregistration.cowin.gov.in/`;
+    if (slotData.age == 18 && U45GroupId){
+        await sendMessage(message, U45GroupId, U45BotId);
+    } else if (slotData.age == 45 && A45GroupId){
+        await sendMessage(message, A45GroupId, A45BotId);
+    }
   } catch (error) {
     console.log("Error while sending message", error);
   }
-
 }
 
-async function createTelegramClient() {
+async function sendMessage(message, groupId, botId){
+  if (!botId || !groupId){
+    throw new Error("Bot or group Id is not set check config, message could not be sent.")
+  }
+  
+  const req = "https://api.telegram.org/bot"+encodeURIComponent(botId)+"/sendMessage?chat_id="+encodeURIComponent(groupId)+"&text="+encodeURIComponent(message); 
+  var response = await got(req);
+  if (response.statusCode == 429){
+    console.log("Getting too many reqeusts error from telegram api");
+  }else if (response.statusCode != 200){
+    throw new Error("Got error message from telegram api with status code "+response.statusCode);
+  }
+}
+
+
+async function main(event) {
   try {
-    const stringSession = new StringSession('');
-    client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
-    await client.start({
-      botAuthToken: BotToken
-    });
-  } catch (e) {
-    console.log("Error while sending message", e);
-  }
-}
-
-function shouldProcessResponse(response){
-  return response.statusCode == 200;
-}
-
-async function getAndProcessDataForPune() {
-  let response = await getDistrictData(363);
-  if (shouldProcessResponse(response)) {
-    //fs.writeFileSync("./response.json", JSON.stringify(response.body));
-    //await filter18PlusAvailable(response, "U45Pune");
-    await filterAvailable(response.body, "U45Pune", 18);
-    await filterAvailable(response.body, "Above45PuneCity", 45, (center, session) => {
-      return (('' + center.pincode).startsWith("411") > 411000 && session.min_age_limit == 45 && session.available_capacity > 0);
-    });
-  }
-}
-
-async function getAndProcessDataForNashik() {
-  let response = await getDistrictData(389);
-  if (shouldProcessResponse(response)){
-    await filterAvailable(response.body, "U45Nashik",18);
-  }
-}
-
-async function getAndProcessDataForNagar() {
-  let response = await getDistrictData(391);
-  if (shouldProcessResponse(response)){
-    await filterAvailable(response.body, "U45Ahmednagar",18);
-  }
-}
-
-async function getAndProcessDataForAbad() {
-  let response = await getDistrictData(397);
-  if (shouldProcessResponse(response)){
-    await filterAvailable(response.body, "U45Aurangabad",18);
-  }
-}
-
-
-function clearLast10MinuteData() {
-  console.log("Clearing 10 minute data");
-  let keys = notificationSentCenters.keys();
-  notificationSentCenters.clear();
-  for (let sessionId of keys){
-    if (notificationsSentPerSessionPer24Hrs.get(sessionId)){
-      let count = notificationsSentPerSessionPer24Hrs.get(sessionId);
-      count++;
-      notificationsSentPerSessionPer24Hrs.set(key, count);
-    } else {
-      let count = 1;
-      notificationsSentPerSessionPer24Hrs.set(key, count);
+    if (!isInited) {
+      init();
     }
+    var districtResponseData = await getDistrictData();
+    await filterAvailableAndSendNotifications(districtResponseData.body);
+  } catch (e) {
+    console.log(e);
   }
+  return;
+};
 
-}
+exports.handler=main;
 
-function clearLast24HourData(){
-  console.log("Clearing 24 hr counts");
-  notificationsSentPerSessionPer24Hrs.clear();
-}
-
-
-async function main() {
-
-  await createTelegramClient();
-  console.log("Created telegram client");
-  setInterval(getAndProcessDataForPune, 7000);
-  setInterval(getAndProcessDataForAbad, 7000);
-  setInterval(getAndProcessDataForNashik, 7000);
-  setInterval(getAndProcessDataForNagar, 7000);
-  setInterval(clearLast10MinuteData, 600000);
-  setInterval(clearLast24HourData, 86400000);
-}
-
-main()
+//main({});
